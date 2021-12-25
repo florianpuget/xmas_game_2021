6cacdacfe56855e1d2700955f3815b9d1404ac65
Florian Puget <florianpuget@Florians-MacBook-Pro.local>
2021-12-25 20:39:32 +0100

    countdown with alarm, fix evaluation, change win song

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-var step, player1, player2, nums, target, textnums, p1answer, p1total, p1numbers, p2answer, p2total, p2numbers, score, p1score, p2score, round, nRounds, winner;
+var step, player1, player2, nums, target, textnums, p1answer, p1total, p1numbers, p2answer, p2total, p2numbers, score, p1score, p2score, round, nRounds, winner, timeLeft, timerId, timeToPlay;
 var xmas_song = new Audio('Love-to-Sing-Chior-Jingle-Bells-Christmas-SongsCarols-k5.mp3');
-var winner_song = new Audio('WinnerPL.mp3');
+var winner_song = new Audio('tada.mp3');
+var alarm = new Audio('mixkit-sound-alert-in-hall-1006.mp3');
 
 function onLoad() {
     step = 0;
@@ -9,7 +10,7 @@ function onLoad() {
     p1score = 0;
     p2score = 0;
     nRounds = 5;
-    var1 = var2 = var3 = var4 = 0;
+    timeToPlay = 30;
 }
 
 window.onload = onLoad;
@@ -25,6 +26,10 @@ function getInput() {
 
 function clearInput() {
     document.getElementById("input").value = "";
+}
+
+function clearInputPlaceholder() {
+    document.getElementById('input').placeholder = "";
 }
 
 function setErrorText(text) {
@@ -70,13 +75,33 @@ function isSubset(arr1, arr2)
 	return (i < n) ? false : true;
 }
 
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+        alarm.play();
+    } else {
+        document.getElementById('time').innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+    }
+}
+
+function clearCountdown() {
+    clearTimeout(timerId);
+    alarm.pause();
+    alarm.currentTime = 0;
+    document.getElementById('time').innerHTML = "";
+}
+
 function game() {
     console.log(`step ${step}`)
     switch (step) {
         case 0:
+            clearInputPlaceholder();
             setInstruction("Welcome to the game!\n\n" +
-            "Rules: make calculation to get closest to target number\n" +
-            "Example: if numbers are 2, 3, 5, 7 and target is 24, you can answer (5+7)*2\n");
+            "Rules: make calculation to get closest to target number\n\n" +
+            "Example: if available numbers are 2, 3, 5, 7 and target is 24, you can answer (5+7)*2");
+            winner_song.pause();
+            winner_song.currentTime = 0;
             xmas_song.play();
             break;
         case 1:
@@ -102,14 +127,24 @@ function game() {
             if (input)
                 nRounds = parseInt(input);
             round = 1;
-            setInstruction("Welcome " + player1 + " & " + player2 + " !\n\n" +
-            "Are you ready?");
+            setInstruction("Enter countdown duration in seconds (default: 30)");
             break;
         case 5:
+            var input = getInput();
+            clearInput();
+            if (input)
+                timeToPlay = parseInt(input);
+            setInstruction(player1 + " are you ready?");
+            break;
+        case 6:
             xmas_song.pause();
             xmas_song.currentTime = 0;
             winner_song.pause();
             winner_song.currentTime = 0;
+
+            timeLeft = timeToPlay;
+            timerId = setInterval(countdown, 1000);
+
             target = Math.floor(Math.random() * 100) + 1;
             nums = [];
             nums.push(Math.floor(Math.random() * 9) + 1);
@@ -122,37 +157,23 @@ function game() {
 
             setInstruction(score + textnums + player1 + " it's your turn");
             break;
-        case 6:
+        case 7:
             p1answer = getInput();
-            p1total = math.round(math.evaluate(p1answer)*10)/10;
+            try {
+                p1total = math.round(math.evaluate(p1answer)*10)/10; // round*10/10 to keep only 1 decimal
+            } catch {
+                step = 6;
+                setErrorText("Not a valid math expression");
+                break;
+            }
             if (isNaN(p1total)) {
-                step = 5;
+                step = 6;
                 setErrorText("Not a valid math expression");
                 break;
             }
             else {
                 p1numbers = p1answer.match(/\d+/g).map(Number);
                 if (!isSubset(nums, p1numbers)) {
-                    step = 5;
-                    setErrorText("Numbers used are not part of available selection");
-                    break;
-                }
-            }
-            clearInput();
-            setErrorText();
-            setInstruction(score + textnums + player2 + " it's your turn");
-            break;
-        case 7:
-            p2answer = getInput();
-            p2total = math.round(math.evaluate(p2answer)*10)/10;
-            if (isNaN(p2total)) {
-                step = 6;
-                setErrorText("Not a valid math expression");
-                break;
-            }
-            else {
-                p2numbers = p2answer.match(/\d+/g).map(Number);
-                if (!isSubset(nums, p2numbers)) {
                     step = 6;
                     setErrorText("Numbers used are not part of available selection");
                     break;
@@ -160,9 +181,42 @@ function game() {
             }
             clearInput();
             setErrorText();
-            setInstruction("Ready for the results?");
+            clearCountdown();
+            setInstruction(player2 + " are you ready?");
             break;
         case 8:
+            timeLeft = timeToPlay;
+            timerId = setInterval(countdown, 1000);
+            setInstruction(score + textnums + player2 + " it's your turn");
+            break;
+        case 9:
+            p2answer = getInput();
+            try {
+                p2total = math.round(math.evaluate(p2answer)*10)/10; // round*10/10 to keep only 1 decimal
+            } catch {
+                step = 8;
+                setErrorText("Not a valid math expression");
+                break;
+            }
+            if (isNaN(p2total)) {
+                step = 8;
+                setErrorText("Not a valid math expression");
+                break;
+            }
+            else {
+                p2numbers = p2answer.match(/\d+/g).map(Number);
+                if (!isSubset(nums, p2numbers)) {
+                    step = 8;
+                    setErrorText("Numbers used are not part of available selection");
+                    break;
+                }
+            }
+            clearInput();
+            setErrorText();
+            clearCountdown();
+            setInstruction("Ready for the results?");
+            break;
+        case 10:
             p1margin = math.abs(target - p1total);
             p2margin = math.abs(target - p2total);
             var result;
@@ -199,20 +253,20 @@ function game() {
             setInstruction(result + score + textnums + p1proposal + p2proposal);
             round++;
             if (round <= nRounds)
-                step = 4;
+                step = 5;
             break;
-        case 9:
+        case 11:
             if (p1score > p2score)
                 winner = player1 + " is the Winner !";
             else if (p1score < p2score)
                 winner = player2 + " is the Winner !";
             else
                 winner = "That's a draw !"
+            
+            winner_song.play();
             setInstruction("GAME FINISHED\n\n" + winner);
             step = -1;
             break;
-        case 10:
-
     }
 
     step++;
